c8cb12a0cd29cb40e53ddaea1d00dd85c43614eb
Charly Gomez <charly.gomez@gmx.at>
2022-09-14 21:12:11 +0200

    chore: project setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  const [photos, setPhotos] = React.useState([]);
+  return (
+    <div>
+      <h1>Photos App</h1>
+    </div>
+  )
+}
+
+export default App
