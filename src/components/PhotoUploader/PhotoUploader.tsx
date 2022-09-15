9380748b24fd1ee238bf7a775c378748dd543896
Charly Gomez <charly.gomez@gmx.at>
2022-09-15 10:25:12 +0200

    feat: photo uploader

---
+++ src/components/PhotoUploader/PhotoUploader.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { PhotoPreview } from "../PhotoPreview";
+
+interface Props {
+  handleUpload: ([photos]: any[]) => void;
+}
+
+export const PhotoUploader = ({ handleUpload }: Props): JSX.Element => {
+  const [tempFiles, setTempFiles] = React.useState<File[]>([]);
+
+  const handleClickUpload = React.useCallback(() => {
+    handleUpload(tempFiles);
+    setTempFiles([]);
+  }, [handleUpload, tempFiles]);
+
+  const handleDelete = React.useCallback((toDeleteFileName: string) => {
+    setTempFiles(
+      (prev) => 
+        prev.filter((current) => current.name !== toDeleteFileName)
+    );
+  }, []);
+
+  return (
+    <>
+      <div>{tempFiles.map((f) => <PhotoPreview photo={f} key={f.name} handleDelete={handleDelete} />)}</div>
+      <label>
+        <div style={{ height: '50px', border: '1px dashed black' }}>
+          Click here to choose files
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={(e) =>
+            setTempFiles(Array.from(e.target.files || []))
+          }
+          multiple
+        />
+      </label>
+      <button
+        disabled={tempFiles.length < 1}
+        onClick={handleClickUpload}
+      >
+        Upload
+      </button>
+    </>
+  );
+}
