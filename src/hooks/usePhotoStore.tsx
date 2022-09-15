baeb91cd742f32630cf20aeca7ed845ea618320f
Charly Gomez <charly.gomez@gmx.at>
2022-09-15 10:53:26 +0200

    feat: hook for photo storage

---
+++ src/hooks/usePhotoStore.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const usePhotoStore = () => {
+  const [photos, setPhotos] = React.useState<File[]>([]);
+
+  const handleUpload = React.useCallback((photos: File[]) => {
+    setPhotos((prev) => [...prev, ...photos]);
+  }, []);
+
+  const handleDelete = React.useCallback((toDeleteFileName: string) => {
+    setPhotos((prev) => prev.filter((current) => current.name !== toDeleteFileName));
+  }, [])
+
+  return {
+    photos,
+    handleUpload,
+    handleDelete,
+  }
+};
